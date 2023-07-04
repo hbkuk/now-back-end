5fa47c5a5129f8a469264375972fc15b0653bcc1
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-04 11:58:59 +0900

    fix, docs, feat: 빌더 패턴 어노테이션 변경, 메서드 javadoc 추가, 메서드 추가
    상속을 받는 하위 클래스에서 부모 클래스인 Post의 빌더 패턴을 사용하도록 어노테이션을 변경
    Post 객체 내 메서드에 javadoc 추가
    하위 클래스에서 getter 사용을 하지 않고, 상위 클래스에서 선언한 메서드 호출을 통한 필드 접근을 위한 메서드 추가

--- src/main/java/com/now/domain/post/Post.java
+++ src/main/java/com/now/domain/post/Post.java
@@ -6,6 +6,7 @@ import com.now.domain.user.User;
 import com.now.exception.CannotDeletePostException;
 import com.now.exception.CannotUpdatePostException;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -15,15 +16,15 @@ import java.util.List;
 /**
  * 게시글 정보를 담고있는 도메인 객체
  *
- * @Builder(toBuilder = true)
- * : 빌더 패턴을 사용하여 객체를 생성합니다. toBuilder 옵션은 생성된 빌더 객체를 이용해 기존 객체를 복사하고 수정할 수 있도록 합니다.
+ * @SuperBuilder(toBuilder = true) : 상속 구조에서 부모 클래스의 빌더 패턴을 자동으로 생성해주는 기능.
+ * toBuilder 옵션은 생성된 빌더 객체를 이용해 기존 객체를 복사하고 수정할 수 있도록 합니다.
  * @ToString : 객체의 문자열 표현을 자동으로 생성합니다. 주요 필드들의 값을 포함한 문자열을 반환합니다.
  * @Getter : 필드들에 대한 Getter 메서드를 자동으로 생성합니다.
  * @NoArgsConstructor(force = true)
  * : 매개변수가 없는 기본 생성자를 자동으로 생성합니다. MyBatis 또는 JPA 라이브러리에서는 기본 생성자를 필요로 합니다.
  * @AllArgsConstructor : 모든 필드를 매개변수로 받는 생성자를 자동으로 생성합니다.
  */
-@Builder(toBuilder = true)
+@SuperBuilder(toBuilder = true)
 @ToString
 @Getter
 @NoArgsConstructor(force = true)
@@ -111,7 +112,7 @@ public class Post {
      * @return 게시글을 삭제할 수 있다면 true 반환, 그렇지 않다면 false 반환
      */
     public boolean canDelete(User user, List<Comment> comments) {
-        if (!user.sameUserId(this.userId)) {
+        if (!user.isSameUserId(this.userId)) {
             throw new CannotDeletePostException("다른 사용자가 작성한 게시글을 삭제할 수 없습니다.");
         }
         
@@ -124,11 +125,26 @@ public class Post {
         return true;
     }
 
+    /**
+     * 게시글을 수정할 수 있다면 true를 반환, 그렇지 않다면 예외를 던짐
+     * 
+     * @param user      유저 정보가 담긴 객체
+     * @return          게시글을 수정할 수 있다면 true를 반환, 그렇지 않다면 예외를 던짐
+     */
     public boolean canUpdate(User user) {
-        if (!user.sameUserId(this.userId)) {
+        if (!user.isSameUserId(this.userId)) {
             throw new CannotUpdatePostException("다른 사용자가 작성한 게시글을 수정할 수 없습니다.");
         }
         return true;
     }
-}
 
+    /**
+     * 전달된 유저 객체의 메서드를 호출해 현재 객체 필드의 userId와 동일하다면 true 반환, 그렇지 않다면 false 반환
+     *
+     * @param user      유저 정보가 담긴 객체
+     * @return          현재 객체 필드의 userId와 동일하다면 true 반환, 그렇지 않다면 false 반환
+     */
+    public boolean isSameUserId(User user) {
+        return user.isSameUserId(this.userId);
+    }
+}
