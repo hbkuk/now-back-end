c4c929f9f7a10b785e409d92ea9ce56c09993306
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-04 12:02:25 +0900

    feat: Inquiry 도메인 객체에서 던지는 예외 클래스 추가

---
+++ src/main/java/com/now/exception/CannotViewInquiryException.java
@@ -0,0 +1,7 @@
+package com.now.exception;
+
+public class CannotViewInquiryException extends RuntimeException {
+    public CannotViewInquiryException(String message) {
+        super(message);
+    }
+}
