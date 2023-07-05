7e15d3458404b84f439172d9b9e6520391d5fbf9
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-05 21:08:44 +0900

    feat: Mapper로 명시된 interface를 스캔하도록 어노테이션 추가

--- src/main/java/com/now/NowApplication.java
+++ src/main/java/com/now/NowApplication.java
@@ -1,9 +1,11 @@
 package com.now;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.now.mapper")
 public class NowApplication {
 
     public static void main(String[] args) {
