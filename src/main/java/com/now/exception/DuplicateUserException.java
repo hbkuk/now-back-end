0707a26c65101372a6f29a87ee20957089eb623e
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-06 20:43:50 +0900

    docs: 사용자 정의 예외 클래스에 javadoc 추가

--- src/main/java/com/now/exception/DuplicateUserException.java
+++ src/main/java/com/now/exception/DuplicateUserException.java
@@ -2,6 +2,9 @@ package com.now.exception;
 
 import java.util.List;
 
+/**
+ * 중복되는 유저 정보가 하나라도 존재한다면 던져지는 Unchecked Exception.
+ */
 public class DuplicateUserException extends RuntimeException {
 
     private List<String> messages;
