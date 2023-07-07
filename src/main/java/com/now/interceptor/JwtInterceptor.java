1a2d2cf569064bd51f8b0e0d14cdd06e09ddae75
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-07 22:43:12 +0900

    feat: JWT 토큰의 처리 담당하는 인터셉터 객체와 인터셉터를 등록하는 객체 추가
    인증된 사용자의 토큰에서 클레임 값을 추출 후 HttpServletRequest의 속성으로 설정하는 인터셉터 객체
    특정 URL 패턴에 대한 토큰 검증을 수행하는 인터셉터 등록 객체

---
+++ src/main/java/com/now/interceptor/JwtInterceptor.java
@@ -0,0 +1,38 @@
+package com.now.interceptor;
+
+import com.now.security.JwtTokenService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * HandlerInterceptor 인터페이스를 구현하여 JWT 토큰의 처리 담당
+ * 인증된 사용자의 토큰에서 클레임 값을 추출 후 HttpServletRequest의 속성으로 설정
+ */
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenService jwtTokenService;
+
+    public JwtInterceptor(JwtTokenService jwtTokenService) {
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    /**
+     * 요청 처리 전에 실행되는 메서드
+     * 토큰의 클레임 값을 추출하여 HttpServletRequest의 속성으로 설정
+     *
+     * @param request  현재 요청을 나타내는 HttpServletRequest 객체
+     * @param response 현재 응답을 나타내는 HttpServletResponse 객체
+     * @param handler  현재 요청을 처리할 핸들러 객체
+     * @return 요청 처리 여부를 나타내는 boolean 값
+     * @throws Exception 예외가 발생한 경우
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute("userId", jwtTokenService.getClaim(request.getHeader("Authorization"), "userId"));
+        return true;
+    }
+}
