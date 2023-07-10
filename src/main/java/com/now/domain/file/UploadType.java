d3cb3a0b2d17f833e0a57ded7a66d03b1a8a0f5e
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-10 22:36:15 +0900

    refactor: 기존 분리된 enum을 하나의 enum으로 병합 및  최대 업로드 횟수 속성 추가
    기존 확장자 그룹을 나타내는 enum인 FileExtensionType과 파일 업로드 크기를 나타내는 상수값을 정의한 enum인 FileSizeType을 하나의 enum으로 병합
    최대 업로드 횟수를 의미하는 maxUploadCount 속성 추가

---
+++ src/main/java/com/now/domain/file/UploadType.java
@@ -0,0 +1,32 @@
+package com.now.domain.file;
+
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * 파일 업로드 타입을 나타내는 enum
+ */
+@Getter
+public enum UploadType {
+    FILE(List.of("jpg", "gif", "png", "zip"), 2048000, 5),
+    IMAGE(List.of("jpg", "gif", "png"), 1024000, 20);
+
+    private final List<String> allowedExtensions;
+    private final int maxUploadSize;
+    private final int maxUploadCount;
+
+    /**
+     * UploadType 객체를 초기화하는 생성자
+     *
+     * @param allowedExtensions 허용하는 파일 확장자 목록
+     * @param maxUploadSize 파일의 최대 업로드 크기 (바이트 단위)
+     * @param maxUploadCount 최대 업로드 횟수
+     */
+    UploadType(List<String> allowedExtensions, int maxUploadSize, int maxUploadCount) {
+        this.allowedExtensions = allowedExtensions;
+        this.maxUploadSize = maxUploadSize;
+        this.maxUploadCount = maxUploadCount;
+    }
+}
+
