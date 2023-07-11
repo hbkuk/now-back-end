fa05acb90912185cfdd5fd783c5ee8f6f353e0d5
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-11 21:55:54 +0900

    fix: File 도메인 객체 관련 원시값 포장 객체의 어노테이션 추가
    ToString, Getter, @NoArgsConstructor, @AllArgsConstructor 추가

--- src/main/java/com/now/domain/file/OriginalFileName.java
+++ src/main/java/com/now/domain/file/OriginalFileName.java
@@ -1,8 +1,24 @@
 package com.now.domain.file;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * 원본 파일 이름을 나타내는 원시값 포장 객체
+ *
+ * @ToString
+ *     : 객체의 문자열 표현을 자동으로 생성합니다. 주요 필드들의 값을 포함한 문자열을 반환합니다.
+ * @Getter
+ *     : 필드들에 대한 Getter 메서드를 자동으로 생성합니다.
+ * @NoArgsConstructor(force = true)
+ *     : 매개변수가 없는 기본 생성자를 자동으로 생성합니다. MyBatis 또는 JPA 라이브러리에서는 기본 생성자를 필요로 합니다.
+ * @AllArgsConstructor
+ *     : 모든 필드를 매개변수로 받는 생성자를 자동으로 생성합니다.
  */
+@ToString
+@Getter
+@NoArgsConstructor(force = true)
 public class OriginalFileName {
     private static final int MAX_VALUE_LENGTH = 500;
     private final String originalFileName;
