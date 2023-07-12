fbf0fc01b7466f4e27a64b115bc970a8df3485ed
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-12 15:45:43 +0900

    docs: Category enum javadoc 추가

--- src/main/java/com/now/domain/post/Category.java
+++ src/main/java/com/now/domain/post/Category.java
@@ -21,7 +21,13 @@ public enum Category implements EnumMapperType {
     SERVICE("서비스"),
     TECHNOLOGY("기술");
 
-    @JsonCreator
+    /**
+     * 전달받은 값으로부터 해당하는 Category enum 생성
+     *
+     * @param value Category의 제목에 해당하는 값
+     * @return 전달받은 값으로부터 해당하는 Category enum
+     */
+    @JsonCreator //  JSON 값을 열거형으로 변환할 때 사용
     public static Category from(String value) {
         for (Category category : Category.values()) {
             if (category.getTitle().equals(value)) {
@@ -31,7 +37,7 @@ public enum Category implements EnumMapperType {
         return null;
     }
 
-    @JsonValue
+    @JsonValue  // 열거형의 특정 필드를 JSON 값으로 변환할 때 사용
     private final String title;
 
     @Override
